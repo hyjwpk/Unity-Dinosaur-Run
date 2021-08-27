828f96b83fe53036ca567e4dfd142b10b37c50c0
taylor-swift-13 <78585172+taylor-swift-13@users.noreply.github.com>
2021-08-27 13:36:57 +0800

    添加死亡动画; 添加障碍物; 添加障碍物恒定速度运动恒定间运动; 添加障碍物回收机制; wc yyds;

---
+++ Assets/Scripts/PlayerDead.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDead : MonoBehaviour
+{
+     private Rigidbody2D _rigidBody2D;
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+          _rigidBody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _animator.SetBool("Dead", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+     void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.name=="ResetTrigger"){ 
+          _animator.SetBool("Jump", false);
+          _animator.SetBool("Dead", true);
+        }
+    }
+}
