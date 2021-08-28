6474a19353d6b5d0183327fdf72b71a3af68ed08
taylor-swift-13 <78585172+taylor-swift-13@users.noreply.github.com>
2021-08-28 13:11:27 +0800

    修改分数表现 修改障碍物生成机制 增加速度随分数变化机制 wc yyds

---
+++ Assets/Scripts/ObstacleGenerateEdge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerateEdge : MonoBehaviour
+{
+      void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "ResetTrigger")
+        {
+            ObstacleGenerator.Instance.GenerateObstacles();
+        }
+    }
+}
--- Assets/Scripts/ObstacleResetEdge.cs
+++ Assets/Scripts/ObstacleResetEdge.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleEdge : MonoBehaviour
+public class ObstacleResetEdge : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "ResetTrigger")
         {
             ObstacleGenerator.Instance.ResetObstacles();
-            ObstacleGenerator.Instance.GenerateObstacles();
         }
     }
 }
